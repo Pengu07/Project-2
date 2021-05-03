3fc46f2002aecf424e800bbe77eb812abff67bb9
Eduda98 <eduda@hawk.iit.edu>
2021-05-02 20:46:05 -0500

    working on js

--- assets/js/site.js
+++ assets/js/site.js
@@ -14,5 +14,28 @@ slides[slideIndex-1].style.display = "block";
 setTimeout(showSlides, 6000);
 }
 
+function validateForm() {
+if (document.contact_form.name.value == "" ){
+	alert( "Please type your name." );
+	document.contactform.name.focus() ;
+	document.getElementbyID("submit").disabled = true;
+}
+if (document.contact_form.email.value == "" ){
+	alert( "Please type your E-Mail." );
+	document.contactform.email.focus() ;
+	document.getElementbyID("submit").disabled = true;
+}
+if (document.contact_form.subject.value == "" ){
+	alert( "Please type your subject." );
+	document.contactform.subject.focus() ;
+	document.getElementbyID("submit").disabled = true;
+}
+if (document.contact_form.mes.value == "" ){
+	alert( "Please type your message." );
+	document.contactform.mes.focus() ;
+document.getElementbyID("submit").disabled = true;
+}
 
-
+else{
+document.getElementbyID("submit").disabled = false;}
+}
